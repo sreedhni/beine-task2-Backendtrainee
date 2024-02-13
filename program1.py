c045dd0b61a8cc8ce6c5184501ac4530b9c552da
sreedhini <sreedhinisree@gmail.com>
2024-02-13 09:50:43 +0530

    second commit

--- program1.py
+++ program1.py
@@ -7,8 +7,14 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-number = int(input("enter a number"))
+number = int(input("enter a number ; "))
 result = factorial(number)
 print(f"The factorial of {number} is {result}")
 
-    
+# If the user enters 3, the factorial() function will be called with n = 3.
+# The function calculates 3 * factorial(2).
+# factorial(2) calculates 2 * factorial(1).
+# This continues until factorial(0) is reached, which returns 1.
+# So, factorial(1) returns 1 * 1 = 1, factorial(2) returns 2 * 1 = 2, factorial(3) returns 3 * 2 = 6
+# Finally, factorial(3) returns 3 * 2 * 1 = 6.
+# The program prints: "The factorial of 3 is 6".
--- program10.py
+++ program10.py
@@ -1,8 +1,12 @@
 # Define a function that accepts lowercase words and returns uppercase words. 
 
-def lowercase_to_uppercase(word):
-    return word.upper()
+def uppercase_lowercase_word():
+    while True:
+        word = input("Enter a lowercase word: ")
+        if word.islower(): 
+            return word.upper()
+        else:
+            print("Please enter a word in lowercase letters.")
 
-lowercase_word = input("Enter a lowercase word: ")
-uppercase_word = lowercase_to_uppercase(lowercase_word)
-print(f"Uppercase word: {uppercase_word}")
+output_word = uppercase_lowercase_word()
+print(output_word) 
